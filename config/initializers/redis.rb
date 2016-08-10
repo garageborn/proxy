b55a2aa9e6f463098dcb55ac45b069f01c9d722b
Alexandre Bini <alexandrebini@gmail.com>
2016-08-09 22:40:49 -0300

    env vars

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,7 @@ require 'redis-namespace'
 require 'ohm'
 require 'ohm/contrib'
 
-REDIS_URL = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379')
+REDIS_URL = ENV.fetch('PROXY_REDIS_URL', 'redis://127.0.0.1:6379')
 REDIS_NAMESPACE = "proxy_#{ Sinatra::Base.environment }".to_sym
 
 redis_connection = Redic.new(REDIS_URL)
