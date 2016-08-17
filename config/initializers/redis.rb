cdda02229728d5cba25201c9835f70db3f995dfe
Alexandre Bini <alexandrebini@gmail.com>
2016-08-16 22:39:04 -0300

    redis timeout

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,7 +5,8 @@ require 'ohm/contrib'
 
 REDIS_URL = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379')
 REDIS_NAMESPACE = "proxy_#{ Sinatra::Base.environment }".to_sym
+REDIS_TIMEOUT = ENV.fetch('REDIS_TIMEOUT', 10_000).to_i * 1_000 # microseconds
 
-redis_connection = Redic.new(REDIS_URL)
+redis_connection = Redic.new(REDIS_URL, REDIS_TIMEOUT)
 namespaced_redis = Redis::Namespace.new(REDIS_NAMESPACE, redis: redis_connection, warning: false)
 Ohm.redis = namespaced_redis
