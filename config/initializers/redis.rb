f70c1c32cbb92370ed939f18930d4180c1c149bf
Alexandre Bini <alexandrebini@gmail.com>
2016-08-31 12:33:50 -0300

    ignore rack timeout

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,10 +3,17 @@ require 'redis-namespace'
 require 'ohm'
 require 'ohm/contrib'
 
-REDIS_URL = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379')
-REDIS_NAMESPACE = "proxy_#{ Sinatra::Base.environment }".to_sym
+REDIS_URL = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379').freeze
+REDIS_NAMESPACE = ENV['PROXY_REDIS_NAMESPACE'].freeze
 REDIS_TIMEOUT = ENV.fetch('REDIS_TIMEOUT', 10_000).to_i * 1_000 # microseconds
 
 redis_connection = Redic.new(REDIS_URL, REDIS_TIMEOUT)
-namespaced_redis = Redis::Namespace.new(REDIS_NAMESPACE, redis: redis_connection, warning: false)
-Ohm.redis = namespaced_redis
+if REDIS_NAMESPACE
+  Ohm.redis = Redis::Namespace.new(
+    REDIS_NAMESPACE.to_sym,
+    redis: redis_connection,
+    warning: false
+  )
+else
+  Ohm.redis = redis_connection
+end
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,15 @@
 require 'raven'
 
-if Sinatra::Base.production?
+PROXY_SENTRY_DSN = ENV['PROXY_SENTRY_DSN'].freeze
+if PROXY_SENTRY_DSN
   Raven.configure do |config|
-    config.dsn = ENV.fetch('PROXY_SENTRY_DSN')
-    config.environments = %w(production)
-    config.tags = { environment: Sinatra::Base.environment }
-    config.processors = [Raven::Processor::SanitizeData]
     config.async = lambda do |event|
       Thread.new { Raven.send(event) }
     end
+    config.dsn = PROXY_SENTRY_DSN
+    config.environments = %w(production)
+    config.excluded_exceptions << 'Rack::Timeout::RequestTimeoutException'
+    config.processors = [Raven::Processor::SanitizeData]
+    config.tags = { environment: Sinatra::Base.environment }
   end
 end
