2b545598ac6ec7452693ca96d76f2d1f8448cbae
Alexandre Bini <alexandrebini@gmail.com>
2017-01-25 16:36:31 -0200

    scheduler

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,8 +20,8 @@ set :rbenv_roles, :all
 
 # whenever
 set :whenever_identifier, -> { fetch(:application) }
-set :whenever_roles, -> { :worker }
-set :whenever_config, -> { "#{ release_path }/config/schedule/schedule.rb" }
+set :whenever_roles, -> { :scheduler }
+set :whenever_config, -> { "#{ release_path }/config/schedule.rb" }
 set :whenever_command, -> {
   [:bundle, :exec, :whenever, "--load-file #{ fetch(:whenever_config) }"]
 }
