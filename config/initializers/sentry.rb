ab19bdf63010705093651f58dea4ca95eecaa959
Alexandre Bini <alexandrebini@gmail.com>
2017-04-03 12:54:28 -0300

    fix sentry initializer

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
+require 'raven'
 PROXY_SENTRY_DSN = ENV['PROXY_SENTRY_DSN'].freeze
 
-if defined?(Raven) && PROXY_SENTRY_DSN.present?
+if PROXY_SENTRY_DSN.present?
   Raven.configure do |config|
     config.dsn = PROXY_SENTRY_DSN
   end
